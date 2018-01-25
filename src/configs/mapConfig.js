16689cd4eeca92d47d5deb41b8bc56e716297d23
israelor <israelor@me.com>
2018-01-25 17:15:32 +0200

    Add more tiles.

--- src/configs/mapConfig.js
+++ src/configs/mapConfig.js
@@ -4,8 +4,21 @@ export const mapTiles = [{
 	tile: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
 	id: 1
 },
+	{name: 'OSM Mapnik',
+		tile: 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+		id: 1
+	},
+	{name: 'OSM Landscape',
+		tile: 'http://{s}.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png',
+		id: 1
+	},
 	{
 		name: 'שכבה שנייה',
 		tile: 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png',
+		id: 2
+	},
+	{
+		name: 'Dark Gray Base',
+		tile: 'http://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}',
 		id: 2
 	}];
